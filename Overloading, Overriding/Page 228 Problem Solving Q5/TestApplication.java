018ec23b25d40210b3ce6024201e55ee0fc340d2
Junyao Chan <junyaoxiandingchan@gmail.com>
2020-06-27 16:08:22 +0800

    -

--- Overloading, Overriding/Page 228 Problem Solving Q5/TestApplication.java
+++ Overloading, Overriding/Page 228 Problem Solving Q5/TestApplication.java
@@ -2,16 +2,17 @@ public class TestApplication{
 	
 	public static void main(String[] args){
 
-		Shape testC = new Circle(4);
-		Shape testShphere = new Sphere(4);
-		Shape testCyl = new Cylinder(4,5);
-		Shape testRect = new Rectangle(4,8);
-		Shape testTri = new Triangle(4,8);
+		Shape testCircle = new Circle(4);
+		Shape testSphere = new Sphere(4);
+		Shape testCylinder = new Cylinder(4,5);
+		Shape testRectangle = new Rectangle(4,8);
+		Shape testTriangle = new Triangle(4,8);
 
-		System.out.println(testTri.getArea());
-
-		////make array and display
-
+		System.out.println("Area of Circle: " + Double.toString(testCircle.getArea()));
+		System.out.println("Surface Area of Sphere: " + Double.toString(testSphere.getArea()));
+		System.out.println("Surface Area of Cylinder: " + Double.toString(testCylinder.getArea()));
+		System.out.println("Area of Rectangle: " + Double.toString(testRectangle.getArea()));
+		System.out.println("Area of Triangle: " + Double.toString(testTriangle.getArea()));
 	}
 
 }
